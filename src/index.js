475edce05d4116c7c486bc421b81f21c4dd699b4
nguyenkhanhhuyhe153585 <69160530+area1110@users.noreply.github.com>
2023-04-05 13:25:50 +0700

    Add static file folder & Node-scss

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = 3000;
 
+// Config use static file in folder 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
 app.use(morgan('combined'));
 
 // Template engine:
@@ -16,11 +20,10 @@ app.set('view engine', 'hbs');
 // Custom dir for handlebars
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
+// Map Route with View
 app.get('/', (req, res) => {
   res.render('home');
 });
-
 app.get('/news', (req, res) => {
   res.render('news');
 });
